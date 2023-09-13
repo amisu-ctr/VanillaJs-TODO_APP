0463725a71173f6474c8950c7a4b40a3b439a872
amisu-ctr <hamisurahman@gmail.com>
2023-09-13 16:49:46 +0000

    Added data and persisting

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = '';
+  saveData(); //save data when task is added
 }
+
+listContainer.addEventListener(
+  'click',
+  (e) => {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
+      saveData(); //save data when its checked or unchecked
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove();
+      saveData(); // save new data when a data is deleted
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
